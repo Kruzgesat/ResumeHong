f3633cad46d4ae2a45cc27db18ea98f7deb0c430
Tommy Hong <201745035@itc.ac.kr>
2024-11-04 15:51:42 +0900

    24.11.04
    Generate Resume Web Page Project

---
+++ src/main/java/inhatc/cse/resumehong/ResumeHongApplication.java
@@ -0,0 +1,13 @@
+package inhatc.cse.resumehong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeHongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeHongApplication.class, args);
+    }
+
+}
